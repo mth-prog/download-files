b19781706efb5f2276b5da2fa29fd847cedb9ec5
Mateus dos santos <53189625+mth-prog@users.noreply.github.com>
2021-06-08 12:02:11 -0300

    Update download-file.py

--- code/download-file.py
+++ code/download-file.py
@@ -6,13 +6,13 @@ dir = r'C:\Users\Usuario(a) Master\OneDrive\Área de Trabalho\Teste'
 
 os.makedirs(dir)
 
-def download_file(url, endereço):
+def download_file(url, endereço, pasta):
     respostaDOserver =  requests.get(url)
 
     with open(endereço, 'wb') as output:
         output.write(respostaDOserver.content)
-    shutil.move('teste.pdf' , dir) #mover o arquivo para a pasta desejada
+    shutil.move(endereço , pasta) #mover o arquivo para a pasta desejada
 
 if __name__ == "__main__":
     
-    download_file("https://www.ime.usp.br/~slago/lp-13.pdf", 'teste.pdf')
+    download_file("https://www.ime.usp.br/~slago/lp-13.pdf", 'teste.pdf', dir)
