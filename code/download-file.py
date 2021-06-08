d613e99932a995f9147ae2b94b51ffabfce5d42a
Mateus dos santos <53189625+mth-prog@users.noreply.github.com>
2021-06-08 14:01:42 -0300

    Update download-file.py

--- code/download-file.py
+++ code/download-file.py
@@ -1,6 +1,7 @@
 import shutil
 import requests
 import os
+import keyboard
 
 dir = r'C:\Users\Usuario(a) Master\OneDrive\Área de Trabalho\Teste'
 
@@ -12,7 +13,6 @@ def download_file(url, endereço, pasta):
     with open(endereço, 'wb') as output:
         output.write(respostaDOserver.content)
     shutil.move(endereço , pasta) #mover o arquivo para a pasta desejada
-
-if __name__ == "__main__":
-    
-    download_file("https://www.ime.usp.br/~slago/lp-13.pdf", 'teste.pdf', dir)
+    os.startfile(dir + "/" + endereço) #abre o arquivo 
+  
+download_file("https://www.ime.usp.br/~slago/lp-13.pdf", 'teste.pdf', dir)
