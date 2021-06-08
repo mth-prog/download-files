6f0e0db3d89e9db9203f4bbfec556f337e2a4da3
Mateus dos santos <53189625+mth-prog@users.noreply.github.com>
2021-06-08 11:47:50 -0300

    Update download-file.py

--- code/download-file.py
+++ code/download-file.py
@@ -11,9 +11,8 @@ def download_file(url, endereço):
 
     with open(endereço, 'wb') as output:
         output.write(respostaDOserver.content)
-
+    shutil.move('teste.pdf' , dir) #mover o arquivo para a pasta desejada
 
 if __name__ == "__main__":
     
     download_file("https://www.ime.usp.br/~slago/lp-13.pdf", 'teste.pdf')
-    shutil.move('teste.pdf' , dir)
