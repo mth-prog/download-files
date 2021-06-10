96c304ec409d4c10ee5f66829143aede725d304c
Mateus dos santos <53189625+mth-prog@users.noreply.github.com>
2021-06-10 09:22:50 -0300

    alteração do parametro nome

--- code/download-file.py
+++ code/download-file.py
@@ -1,22 +1,20 @@
 import shutil
 import requests
 import os
-from pynput.keyboard import Key, Controller
+from zipfile import ZipFile
+
 
 dir = r'C:\Users\Usuario(a) Master\OneDrive\Área de Trabalho\Teste'
 
 os.makedirs(dir)
 
-def download_file(url, endereço, pasta):
+def download_file(url, nome, pasta):
     respostaDOserver =  requests.get(url)
 
-    with open(endereço, 'wb') as output:
+    with open(nome, 'wb') as output:
         output.write(respostaDOserver.content)
-    shutil.move(endereço , pasta) #mover o arquivo para a pasta desejada
-    #os.startfile(dir + "/" + endereço) #abre o arquivo 
-  
-download_file("https://drive.google.com/u/0/uc?id=1QEvcAhZaVqgovN6PqltFqOnHq9IepXdz&export=download", "teste.accdb", dir)
+    shutil.move(nome , pasta) #mover o arquivo para a pasta desejada
+    # os.startfile(dir + "/" + endereço) #abre o arquivo 
+    
+download_file("https://drive.google.com/u/0/uc?id=1QEvcAhZaVqgovN6PqltFqOnHq9IepXdz&export=download", "teste.zip", dir)
 
-'''keyb = Controller()
-
-keyb.press('l')'''
