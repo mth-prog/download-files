ecc3a74b6cf1b241072aaff3c1e7ee463fd8c2b3
Mateus dos santos <53189625+mth-prog@users.noreply.github.com>
2021-06-10 11:26:42 -0300

    extrai o zip

--- code/download-file.py
+++ code/download-file.py
@@ -1,12 +1,13 @@
 import shutil
 import requests
 import os
-from zipfile import ZipFile
+import zipfile
 
 
 dir = r'C:\Users\Usuario(a) Master\OneDrive\Área de Trabalho\Teste'
 
 os.makedirs(dir)
+
 
 def download_file(url, nome, pasta):
     respostaDOserver =  requests.get(url)
@@ -14,7 +15,20 @@ def download_file(url, nome, pasta):
     with open(nome, 'wb') as output:
         output.write(respostaDOserver.content)
     shutil.move(nome , pasta) #mover o arquivo para a pasta desejada
-    # os.startfile(dir + "/" + endereço) #abre o arquivo 
+    #os.startfile(dir + "/" + nome) #abre o arquivo 
     
-download_file("https://drive.google.com/u/0/uc?id=1QEvcAhZaVqgovN6PqltFqOnHq9IepXdz&export=download", "teste.zip", dir)
+download_file("https://drive.google.com/u/0/uc?id=1CDGAvfTmCLXfuRwXkWCwSgVBXW0_YD0D&export=download", "teste.zip", dir)
 
+
+dir_name = dir
+extension = ".zip"
+
+os.chdir(dir_name) 
+
+for item in os.listdir(dir_name):
+    if item.endswith(extension):
+        file_name = os.path.abspath(item) 
+        zip_ref = zipfile.ZipFile(file_name)
+        zip_ref.extractall(dir_name)
+        zip_ref.close()
+        os.remove('teste.zip')
